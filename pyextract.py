6b9595a33da6af9a95d5bde7eeaba2c9258f1650
Liam Hawkins <liamhawkinscu@gmail.com>
2016-12-08 19:04:39 -0500

    Add non-functional GUI

--- pyextract.py
+++ pyextract.py
@@ -1,6 +1,7 @@
 import csv
 import sys
-
+from Tkinter import *
+import tkFileDialog
 
 def multiscan(reader, f):
     next(reader)
@@ -31,14 +32,52 @@ def biotek(reader, f):
     f.write('')
     f.close()
 
-if len(sys.argv) != 3:
-    print("Usage: python pyextract.py <inputfile> <outputfile>")
-else:
-    with open(sys.argv[2], 'w') as f:
-        with open(sys.argv[1], 'r') as csvfile:
-            reader = csv.reader(csvfile, delimiter='\t')
-            row1 = next(reader)
-            if row1[0][0] == 'P':
-                multiscan(reader, f)
-            else:
-                biotek(reader, f)
+class Window(Frame):
+
+    def __init__(self, master=None):
+        Frame.__init__(self, master)
+        self.master = master
+        self.init_window()
+
+    def init_window(self):
+        self.master.title("pyextract")
+        self.pack(fill=BOTH, expand=1)
+        fileopenButton = Button(self, text="Import File", command=self.askopenfilename)
+        fileopenButton.place(x=300, y=10)
+        filesaveasButton = Button(self, text="Export File", command=self.asksaveasfilename)
+        filesaveasButton.place(x=300, y=60)
+        convertButton = Button(self, text="Convert", command=self.convert)
+        convertButton.place(x=300, y=110)
+
+        self.file_opt = options = {}
+        options['filetypes'] = [('All Files', '.*')]
+        options['title'] = 'Import File'
+
+    def askopenfilename(self):
+        filename=tkFileDialog.askopenfilename(**self.file_opt)
+        print(filename)
+
+    def asksaveasfilename(self):
+        savefilename = tkFileDialog.asksaveasfilename(**self.file_opt)
+        print(savefilename)
+
+    def convert(self):
+        return
+
+
+gui = Tk()
+gui.geometry("400x150")
+app = Window(gui)
+gui.mainloop()
+
+#if len(sys.argv) != 3:
+#    print("Usage: python pyextract.py <inputfile> <outputfile>")
+#else:
+#    with open(sys.argv[2], 'w') as f:
+#        with open(sys.argv[1], 'r') as csvfile:
+#            reader = csv.reader(csvfile, delimiter='\t')
+#            row1 = next(reader)
+#            if row1[0][0] == 'P':
+#                multiscan(reader, f)
+#            else:
+#                biotek(reader, f)
