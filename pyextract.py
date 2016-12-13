9817ba771b45711c0818aaafb7c98d5482ee39e6
Liam Hawkins <liamhawkinscu@gmail.com>
2016-12-13 10:27:07 -0500

    Add comments

--- pyextract.py
+++ pyextract.py
@@ -4,9 +4,10 @@ from Tkinter import *
 import tkFileDialog
 
 
+# Converts multiscan format to MPA readable
 def multiscan(reader, f):
-    next(reader)
-    next(reader)
+    next(reader)  # Skips first line
+    next(reader)  # Skips second line
     for row in reader:
         if row != [] and row[0] != '':
             if row[0][0] == ' ' or row[0][0] == 'R':
@@ -21,6 +22,7 @@ def multiscan(reader, f):
     f.close()
 
 
+# Converts Biotek format to MPA readable
 def biotek(reader, f):
     for row in reader:
         if row != [] and row[0] != '':
@@ -34,6 +36,7 @@ def biotek(reader, f):
     f.close()
 
 
+# Pop up window indicates when file has successfully converted
 def finishedpopup():
         toplevel = Toplevel()
         toplevel.geometry('300x100')
@@ -42,7 +45,6 @@ def finishedpopup():
 
 
 class Window(Frame):
-
     def __init__(self, master=None):
         Frame.__init__(self, master)
         self.master = master
@@ -51,38 +53,49 @@ class Window(Frame):
     def init_window(self):
         self.master.title('pyextract')
         self.pack(fill=BOTH, expand=1)
+
         fileopenButton = Button(self, text='Import File', command=self.askopenfilename)
         fileopenButton.place(x=300, y=10)
+
         filesaveasButton = Button(self, text='Export File', command=self.asksaveasfilename)
         filesaveasButton.place(x=300, y=60)
+
         convertButton = Button(self, text='Convert', command=self.convert)
         convertButton.place(x=300, y=110)
+
         self.filetext = Entry(self, width=35)
         self.filetext.place(x=0, y=10)
+
         self.savefiletext = Entry(self, width=35)
         self.savefiletext.place(x=0, y=60)
 
+        # Options for Import File dialogue box
         self.file_opt = options = {}
-        options['filetypes'] = [('BioTek/Multiscan Files', '*.txt;*.exp'), ('All files', '*.*')]
+        options['filetypes'] = [('BioTek/Multiscan Files', '*.txt;*.exp'),
+                                ('All files', '*.*')]
         options['title'] = 'Import File'
         options['initialdir'] = os.getcwd()
 
+        # Options for Export File Dialogue box
         self.savefile_opt = options = {}
         options['defaultextension'] = '.txt'
         options['filetypes'] = [('Text Files', '*.txt')]
         options['title'] = 'Export File'
         options['initialdir'] = os.getcwd()
 
+    # Import file, deletes text in text box and replaces with filename selected
     def askopenfilename(self):
         if len(self.filetext.get()) != 0:
             self.filetext.delete('0', END)
         self.filetext.insert(END, tkFileDialog.askopenfilename(**self.file_opt))
 
+    # Export file, deletes text in text box and replaces with selected file
     def asksaveasfilename(self):
         if len(self.savefiletext.get()) != 0:
             self.savefiletext.delete('0', END)
         self.savefiletext.insert(END, tkFileDialog.asksaveasfilename(**self.savefile_opt))
 
+    # Checks for filenames in textboxes, then proceedes with conversion
     def convert(self):
         self.filename = self.filetext.get()
         self.savefilename = self.savefiletext.get()
